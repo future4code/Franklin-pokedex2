9bdadb5bd7265dbb9efe4cb3a8cd7f6bfd054e54
Ruth Pessoa de Lima <98413094+Rtpessoa@users.noreply.github.com>
2022-07-09 18:05:16 -0300

    adicionei a funcao de remover o card quando apertar no botao de adicionar pokemon (#8)

--- pokedex/src/pages/Home/Home.jsx
+++ pokedex/src/pages/Home/Home.jsx
@@ -10,25 +10,31 @@ import {goToPokedex} from "../../routes/coordinator"
 const Home = ({addPokemon, setAddPokemon}) => {
     const navigate = useNavigate()
     const pokemons = useRequestData(`${BASE_URL}/pokemon`, {})
+    
+    
     const addToPokedex = (pokemon) => {
         const newPokemon = [...addPokemon]
         newPokemon.push(pokemon)
         setAddPokemon(newPokemon)  
-    }
-     console.log(addPokemon)
+        let index = pokemons.results.indexOf(pokemon)
+        pokemons.results.splice(index, 1)
+    } 
+
     return(
         <div>
         <Header onClick={() => goToPokedex(navigate)}/>
-        <CardContainer>
+        <CardContainer> 
         {pokemons.results && pokemons.results.map((pokemon) => {
            return ( 
             <PokeCards key={pokemon.url} name={pokemon.name} url={pokemon.url} onClick={() => addToPokedex(pokemon)}
             nameButton='Adicionar a Pokedex'/>        
-        )
-    })}
+        ) 
+    } ) }
         </CardContainer>
+       
         </div>
     )
 }
 
 export default Home
+
