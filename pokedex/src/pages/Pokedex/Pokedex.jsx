7fc2db624672daa34669a683cca3d64b68e3dd0b
Leonardo Santino <leonardosantino@live.com>
2022-07-11 19:35:43 -0300

    feat: create pokedex context

--- pokedex/src/pages/Pokedex/Pokedex.jsx
+++ pokedex/src/pages/Pokedex/Pokedex.jsx
@@ -2,26 +2,32 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 import PokeCards from "../../components/PokeCards";
+import { usePokedexContext } from "../../contexts/PokedexContext";
 import { goToLastPage } from "../../routes/coordinator";
 import { CardContainer } from "../Home/styled";
 
-const Pokedex = ({addPokemon}) => {
-    const navigate = useNavigate()
-    console.log(addPokemon, 'pokedex')
-    return(
-        <div>
-            <Header onClick={() => goToLastPage(navigate)}/>
-        <CardContainer>
-            {addPokemon && addPokemon.map((pokemon) => {
-           return (
-            <PokeCards key={pokemon.url} name={pokemon.name} url={pokemon.url}
-            nameButton='Remover'/>      
-        )   
-    })}
-        </CardContainer>
-        </div>
-    )
-}
+const Pokedex = () => {
+  const navigate = useNavigate();
+  const { pokedex } = usePokedexContext();
 
+  return (
+    <div>
+      <Header onClick={() => goToLastPage(navigate)} />
+      <CardContainer>
+        {pokedex &&
+          pokedex.map((pokemon) => {
+            return (
+              <PokeCards
+                key={pokemon.url}
+                name={pokemon.name}
+                url={pokemon.url}
+                nameButton="Remover"
+              />
+            );
+          })}
+      </CardContainer>
+    </div>
+  );
+};
 
-export default Pokedex
+export default Pokedex;
