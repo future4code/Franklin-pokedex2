345485f8d6b930c1bd5c4356191ab50516874998
Leonardo Santino <leonardosantino@live.com>
2022-07-12 10:04:56 -0300

    feat: create function update pokedex

--- pokedex/src/utils/removePokemon.js
+++
@@ -1,9 +0,0 @@
-export function removePokemon(name, pokedex, setPokedex) {
-  const updatePokedex = [];
-
-  pokedex.forEach((pokemon) => {
-    if (name === !pokemon.name) updatePokedex.push(pokemon);
-  });
-
-  setPokedex(updatePokedex);
-}
